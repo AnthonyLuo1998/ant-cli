30da037716e15978238f33015e54fcb754086fb0
AnthonyLuo1998 <jum1274001055@gmail.com>
2025-03-30 21:44:33 +0800

    test: test

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,7 +1,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"], // 使用常规的 commitlint 配置文件
   rules: {
-    "type-enum": [2, "always", ["feat"]], // 定义允许的提交类型
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ], // 定义允许的提交类型
     "scope-case": [2, "always", "lower-case"], // 强制提交范围为小写
     "subject-case": [0], // 不强制主题大小写（但通常建议使用 sentence-case）
     "subject-empty": [2, "never"], // 禁止空的提交主题
